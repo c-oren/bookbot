6bfcad795de5878fb95868942b16f89ec9d1fc01
Chanan Oren <8764093+c-oren@users.noreply.github.com>
2022-10-30 20:06:02 -0700

    Add report function

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ def count_letters(text):
             letter_counts[l] = letter_counts.get(l, 0) + 1
 
     return letter_counts
+
+def report():
+    print(f"--- Begin report of {file_name} ---")
+    print(f"{count_words(file_contents)} words found in the document\n")
+
+    ltr_count = list(count_letters(file_contents).items())
+    ltr_count.sort(key=lambda x: x[1], reverse=True)
+
+    for k, v in ltr_count:
+        print(f"The '{k}' character was found {v} times")
+
+    print("--- End report ---")
+
+report()
