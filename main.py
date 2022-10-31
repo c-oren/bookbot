806ad03c6e476f12472d232062eaf8e40a6433e6
Chanan Oren <8764093+c-oren@users.noreply.github.com>
2022-10-30 20:04:37 -0700

    Add count_letters function

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ with open(file_name) as f:
 
 def count_words(text):
     return len(text.split())
+
+def count_letters(text):
+    letter_counts = {}
+
+    for l in text.lower():
+        if l.isalpha():
+            letter_counts[l] = letter_counts.get(l, 0) + 1
+
+    return letter_counts
